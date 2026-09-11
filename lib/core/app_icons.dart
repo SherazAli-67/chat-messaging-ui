3c0a396adc4d414842459d7e86a72ad25f74f1ed
sheraz-ali <soomrosheraz054@gmail.com>
2026-09-11 11:46:23 +0500

    Chat Messaging UI - Avatars set

--- lib/core/app_icons.dart
+++ lib/core/app_icons.dart
@@ -1,3 +1,27 @@
 class AppIcons {
+  static const icHome = 'assets/icons/ic_home.svg';
+  static const icProfile = 'assets/icons/ic_profile.svg';
+  static const icAddUser = 'assets/icons/ic_add_user.svg';
+  static const icPlus = 'assets/icons/ic_plus.svg';
+  static const icAdd = 'assets/icons/ic_add.svg';
+  static const icMore = 'assets/icons/ic_more.svg';
+  static const icTickDouble = 'assets/icons/ic_tick_double.svg';
+  static const icTickDoubleAccent = 'assets/icons/ic_tick_double_accent.svg';
+  static const icMessage = 'assets/icons/ic_message.svg';
+  static const icContact = 'assets/icons/ic_contact.svg';
+  static const icUserGroup = 'assets/icons/ic_user_group.svg';
+  static const icAddStoryRing = 'assets/icons/ic_add_story_ring.png';
 
+  static const avatarCaroline = 'assets/avatars/avatar_caroline.png';
+  static const avatarSteve = 'assets/avatars/avatar_steve.png';
+  static const avatarGregory = 'assets/avatars/avatar_gregory.png';
+  static const avatarRosalie = 'assets/avatars/avatar_rosalie.png';
+  static const avatarJulius = 'assets/avatars/avatar_julius.png';
+  static const avatarLaura = 'assets/avatars/avatar_laura.png';
+  static const avatarRafael = 'assets/avatars/avatar_rafael.png';
+  static const avatarKatherine = 'assets/avatars/avatar_katherine.png';
+  static const avatarTerrence = 'assets/avatars/avatar_terrence.png';
+  static const avatarAlyssa = 'assets/avatars/avatar_alyssa.png';
+  static const avatarAndrew = 'assets/avatars/avatar_andrew.png';
+  static const avatarDixie = 'assets/avatars/avatar_dixie.png';
 }
